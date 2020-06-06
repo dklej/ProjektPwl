f9bb53f80fc067e34be2e5541af4e649b45ebae2
dklej <62219498+dklej@users.noreply.github.com>
2020-06-06 12:21:17 +0200

    Update klej.sql

--- klej.sql
+++ klej.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Czas generowania: 02 Cze 2020, 20:00
--- Wersja serwera: 10.1.36-MariaDB
--- Wersja PHP: 7.2.11
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Baza danych: `klej`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `posts`
---
 
 CREATE TABLE `posts` (
   `id_post` int(11) NOT NULL,
@@ -110,6 +82,4 @@ ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_acc`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
