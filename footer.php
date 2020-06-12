e642c4d178499a0abc7af311a84a9a98c78a6aea
dklej <62219498+dklej@users.noreply.github.com>
2020-06-12 11:39:01 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <p id="stopka"> 
-        Strona została stworzona na potrzeby projektu. <br>
-        Wykorzystywanie treści jest dozwolone oraz nie zostaną wyciągnięte żadne konsekwencje.
-        <br>
-        ŻadnaDobraFirma z.o.o <br> Tel: 989-234-222 <br> Fax: 111-122-233
-    </p>
-</footer>
